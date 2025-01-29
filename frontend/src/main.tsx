782746b13100aae889eaf183b69a1fa64c538139
jonathanhu237 <jonathanhu237@gmail.com>
2025-01-29 11:08:26 +0800

    feat: 安装 tanstack router
    Signed-off-by: jonathanhu237 <jonathanhu237@gmail.com>

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,34 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRouter, RouterProvider } from "@tanstack/react-router";
+import { StrictMode, lazy } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { routeTree } from "./routeTree.gen";
 
-createRoot(document.getElementById('root')!).render(
+const router = createRouter({
+  routeTree,
+});
+
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
+
+const TanStackRouterDevtools =
+  process.env.NODE_ENV === "production"
+    ? () => null // Render nothing in production
+    : lazy(() =>
+        // Lazy load in development
+        import("@tanstack/router-devtools").then((res) => ({
+          default: res.TanStackRouterDevtools,
+          // For Embedded Mode
+          // default: res.TanStackRouterDevtoolsPanel
+        }))
+      );
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+    <TanStackRouterDevtools router={router} />
+  </StrictMode>
+);
